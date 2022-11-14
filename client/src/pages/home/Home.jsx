9eea280e43160c919264b3e7b23f3f23af0d4e22
Mohamed Nasr <efmnt2020@gmail.com>
2022-11-13 20:10:33 -0500

    Updated the share button

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,11 +2,13 @@ import Stories from "../../component/stories/Stories";
 import Posts from "../../component/posts/Posts";
 
 import "./home.scss";
+import  Share from '../../component/share/Share'
 
 const Home = () => {
   return (
     <div className="home">
       <Stories />
+      <Share/>
       <Posts />
     </div>
   );
---
+++ client/test.js
@@ -0,0 +1,6 @@
+let employees = ["Lara", "Sukhi", "Evee", "Simi", "Beno", "Jay"];
+let employeeSalary = [1000, 1300, 957.89, 3230.14, 750, 13900];
+
+for (let i = 0; i < employees.length; i++) {
+  console.log(employees[i] + ':' + employeeSalary[i]);
+}
